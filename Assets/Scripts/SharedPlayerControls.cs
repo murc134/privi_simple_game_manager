65f84c520edc6d1aa1c0549e843c515ba3d7ecbc
Christoph Brucksch <c.brucksch@googlemail.com>
2021-06-14 20:13:36 +0200

    Include Fixed Camera ThirdPersonController

--- Assets/Scripts/SharedPlayerControls.cs
+++ Assets/Scripts/SharedPlayerControls.cs
@@ -17,12 +17,7 @@ public class SharedPlayerControls : MonoBehaviour
             {
                 cam = Camera.main;
             }
-            else
-            {
-                Debug.LogWarning(
-                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
-                // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
-            }
+
         }
     }
 
